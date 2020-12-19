534a0c5318444e0dba2d3f6078ab326971c7a1b3
Tamara Salvatori <tamara.salvatori@thoughtworks.com>
2020-12-19 18:43:49 -0300

    First commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        delayWithSeconds(0.2) {
+            sender.alpha = 1
+        }
+    }
+
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+
+    func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            completion()
+        }
+    }
+}
